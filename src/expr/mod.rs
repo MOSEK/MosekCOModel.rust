6bc8856b543563ff0e27e81570b1cfe558b95e0e
ulfw <ulf.worsoe@mosek.com>
2022-09-29 10:36:49 +0200

    working on matrixes

--- src/expr/mod.rs
+++ src/expr/mod.rs
@@ -28,17 +28,14 @@ pub trait ExprTrait : Sized {
     // fn reshape(self, shape : &[usize]) -> ExprReshape<Self>  { ExprReshape{  item : self, shape : shape.to_vec() } }
     // fn mul_scalar(self, c : f64) -> ExprMulScalar<Self> { ExprMulScalar{ item:self, c : c } }
     // fn mul_vec_left(self, v : Vec<f64>) -> ExprMulVec<Self>
-    fn mul_left_dense(self, v : DenseMatrix) -> ExprMulLeftDense<Self> { ExprMulLeftDense{item:self,lhs:v} }
-    fn mul_right_dense(self, v : DenseMatrix) -> ExprMulRightDense<Self> { ExprMulRightDense{item:self,rhs:v} }
+    fn mul_left_dense(self, v : matrix::DenseMatrix) -> ExprMulLeftDense<Self> { ExprMulLeftDense{item:self,lhs:v} }
+    fn mul_right_dense(self, v : matrix::DenseMatrix) -> ExprMulRightDense<Self> { ExprMulRightDense{item:self,rhs:v} }
     // fn transpose(self) -> ExprPermuteAxes<Self>
     // fn axispermute(self) -> ExprPermuteAxes<Self>
-
+    // fn slice(self, range : &[(Range<usize>)])
 
     fn mul<V>(self,other : V) -> V::Result where V : ExprRightMultipliable<Self> { other.mul_right(self) }
-
-    fn add<R:ExprTrait>(self,rhs : R) -> ExprAdd<Self,R> {
-        ExprAdd{lhs:self,rhs}
-    }
+    fn add<R:ExprTrait>(self,rhs : R) -> ExprAdd<Self,R> { ExprAdd{lhs:self,rhs} }
 }
 
 
@@ -186,97 +183,147 @@ pub trait ExprMultiplyableRight {
     fn mul<E:ExprTrait>(self,lhs : E) -> Self::O;
 }
 
-pub trait Matrix {
-    fn size(&self) -> (usize,usize);
-    fn numnz(&self) -> usize;
-    fn issparse(&self) -> bool;
-    fn sparsity(&self,sp : & mut [usize]);
-    fn values<'a>(&self) -> &'a[f64];
 
-    fn new(height : usize, width : usize, data : Vec<f64>) -> DenseMatrix {
-        if height*width != data.len() { panic!("Invalid data size for matrix")  }
-        DenseMatrix{
-            dim  : (height,width),
-            data : data
+/// Trait defining something that can be right-multiplied on an
+/// expression
+pub trait ExprRightMultipliable<E:ExprTrait> {
+    type Result : ExprTrait;
+    fn mul_right(self,other : E) -> Self::Result;
+}
+
+/// Trait defining something that can be left-multiplied on an
+/// expression
+pub trait ExprLeftMultipliable<E:ExprTrait> {
+    type Result : ExprTrait;
+    fn mul(self,other : E) -> Self::Result;
+}
+
+
+mod matrix {
+    //use itertools::{izip};
+    use super::{ExprRightMultipliable,ExprTrait,ExprMulLeftDense,ExprMulRightDense};
+
+    pub fn dense(height : usize, width : usize, data : Vec<f64>) -> DenseMatrix { DenseMatrix::new(height,width,data) }
+    pub fn sparse(height : usize, width : usize,
+                  subi : &[usize],
+                  subj : &[usize],
+                  cof  : &[f64]) -> SparseMatrix {
+        if subi.len() != subj.len() || subi.len() != cof.len() {
+            panic!("Invalid matrix data")
         }
-    }
-    fn ones(height : usize, width : usize) -> DenseMatrix {
-        DenseMatrix{
-            dim : (height,width),
-            data : vec![1.0; height*width]
-        }
-    }
-    fn diag(data : &[f64]) -> SparseMatrix {
+
+        if subi.iter().max().copied().unwrap_or(0) >= height
+            || subj.iter().max().copied().unwrap_or(0) >= width {
+                panic!("Invalid matrix data")
+            }
+
+        let mut perm : Vec<usize> = (0..subi.len()).collect();
+        perm.sort_by_key(|&k| unsafe{(*subi.get_unchecked(k),*subj.get_unchecked(k)) });
+
+        if ! perm.iter().zip(perm[1..].iter()).map(|(&p0,&p1)| unsafe{(*subi.get_unchecked(p0),*subi.get_unchecked(p1),*subj.get_unchecked(p0),*subj.get_unchecked(p1))})
+            .all(|(i0,i1,j0,j1)| i0 < i1 || (i0 == i1 && j0 < j1)) {
+                panic!("Matrix contains duplicates");
+            }
+
         SparseMatrix{
-            dim : (data.len(),data.len()),
-            sp  : (0..data.len()*data.len()).step_by(data.len()+1).collect(),
-            data : data.to_vec()
-        }
-    }
-}
-
-#[derive(Clone)]
-pub struct DenseMatrix {
-    dim  : (usize,usize),
-    data : Vec<f64>
-}
-
-#[derive(Clone)]
-pub struct SparseMatrix {
-    dim  : (usize,usize),
-    sp   : Vec<usize>,
-    data : Vec<f64>,
-}
-
-impl DenseMatrix {
-    pub fn new(height : usize, width : usize, data : Vec<f64>) -> DenseMatrix {
-        if height*width != data.len() { panic!("Invalid data size for matrix")  }
-        DenseMatrix{
             dim : (height,width),
-            data : data
+            sp  : perm.iter().map(|&p| unsafe{*subi.get_unchecked(p)}*width+unsafe{*subj.get_unchecked(p)}).collect(),
+            data : perm.iter().map(|&p| unsafe{*cof.get_unchecked(p)}).collect()
         }
     }
+
+    pub fn from_triplets(height : usize,
+                         width : usize,
+                         data : &[(usize,usize,f64)]) -> SparseMatrix {
+        if data.iter().max_by_key(|&v| v.0).map(|&v| v.0 >= height).unwrap_or(false)
+            || data.iter().max_by_key(|&v| v.1).map(|&v| v.1 >= width).unwrap_or(false) {
+                panic!("Invalid matrix data")
+            }
+
+        let mut perm : Vec<usize> = (0..data.len()).collect();
+        perm.sort_by_key(|&k| { let d = unsafe{*data.get_unchecked(k)}; (d.0,d.1) });
+
+        if ! perm.iter().zip(perm[1..].iter()).map(|(&p0,&p1)| unsafe{(*data.get_unchecked(p0),*data.get_unchecked(p1))})
+            .all(|(i0,i1)| i0.0 < i1.0 || (i0.0 == i1.0 && i0.1 < i1.1)) {
+                panic!("Matrix contains duplicates");
+            }
+        SparseMatrix{
+            dim : (height,width),
+            sp  : perm.iter().map(|&p| { let i = unsafe{data.get_unchecked(p)}; i.0*width+i.1 }).collect(),
+            data : perm.iter().map(|&p| unsafe{data.get_unchecked(p)}.2 ).collect()
+        }
+    }
+
     pub fn ones(height : usize, width : usize) -> DenseMatrix {
         DenseMatrix{
             dim : (height,width),
             data : vec![1.0; height*width]
         }
     }
-    pub fn diag(data : &[f64]) -> DenseMatrix {
-        DenseMatrix{
+    pub fn diag(data : &[f64]) -> SparseMatrix {
+        SparseMatrix{
             dim : (data.len(),data.len()),
-            data : iproduct!((0..data.len()).zip(data.iter()),0..data.len()).map(|((i,&c),j)| if i == j {c} else { 0.0 }).collect()
+            sp  : (0..data.len()*data.len()).step_by(data.len()+1).collect(),
+            data : data.to_vec()
         }
+    }
+
+
+    #[derive(Clone)]
+    pub struct DenseMatrix {
+        dim  : (usize,usize),
+        data : Vec<f64>
+    }
+
+    #[derive(Clone)]
+    pub struct SparseMatrix {
+        dim  : (usize,usize),
+        sp   : Vec<usize>,
+        data : Vec<f64>,
+    }
+
+    impl DenseMatrix {
+        pub fn new(height : usize, width : usize, data : Vec<f64>) -> DenseMatrix {
+            if height*width != data.len() { panic!("Invalid data size for matrix")  }
+            DenseMatrix{
+                dim : (height,width),
+                data : data
+            }
+        }
+        pub fn dim(&self) -> (usize,usize) { self.dim }
+        pub fn height(&self) -> usize { self.dim.0 }
+        pub fn width(&self) -> usize { self.dim.1 }
+        pub fn data(&self) -> &[f64] { self.data.as_slice() }
+    }
+
+    impl<E:ExprTrait> ExprRightMultipliable<E> for DenseMatrix {
+        type Result = ExprMulRightDense<E>;
+        fn mul_right(self,other : E) -> Self::Result { other.mul_right_dense(self) }
+    }
+
+    impl DenseMatrix {
+        fn mul<E:ExprTrait>(self,other : E) -> ExprMulLeftDense<E> { ExprMulLeftDense{item : other,lhs : self} }
     }
 }
 
-pub trait ExprRightMultipliable<E:ExprTrait> {
-    type Result : ExprTrait;
-    fn mul_right(self,other : E) -> Self::Result;
+
+impl<E:ExprTrait> ExprRightMultipliable<E> for f64 {
+    type Result = ExprMulScalar<E>;
+    fn mul_right(self,other : E) -> Self::Result { ExprMulScalar{item : other, lhs : self} }
 }
-
-impl<E:ExprTrait> ExprRightMultipliable<E> for DenseMatrix {
-    type Result = ExprMulRightDense<E>;
-    fn mul_right(self,other : E) -> Self::Result { other.mul_right_dense(self) }
+impl<E:ExprTrait> ExprLeftMultipliable<E> for f64 {
+    type Result = ExprMulScalar<E>;
+    fn mul(self,other : E) -> Self::Result { ExprMulScalar{item : other, lhs : self} }
 }
-
-impl DenseMatrix {
-    fn mul<E:ExprTrait>(self,other : E) -> ExprMulLeftDense<E> { ExprMulLeftDense{item : other,lhs : self} }
-}
-
-
-
-
-
 
 
 pub struct ExprMulLeftDense<E:ExprTrait> {
     item : E,
-    lhs  : DenseMatrix
+    lhs  : matrix::DenseMatrix
 }
 pub struct ExprMulRightDense<E:ExprTrait> {
     item : E,
-    rhs  : DenseMatrix
+    rhs  : matrix::DenseMatrix
 }
 pub struct ExprMulScalar<E:ExprTrait> {
     item : E,
@@ -290,14 +337,14 @@ pub struct ExprDotVec<E:ExprTrait> {
 impl<E:ExprTrait> ExprTrait for ExprMulLeftDense<E> {
     fn eval(&self,rs : & mut WorkStack, ws : & mut WorkStack, xs : & mut WorkStack) {
         self.item.eval(ws,rs,xs);
-        eval::mul_left_dense(self.lhs.data.as_slice(),self.lhs.dim.0, self.lhs.dim.1,rs,ws,xs);
+        eval::mul_left_dense(self.lhs.data(),self.lhs.height(), self.lhs.width(),rs,ws,xs);
     }
 }
 
 impl<E:ExprTrait> ExprTrait for ExprMulRightDense<E> {
     fn eval(&self,rs : & mut WorkStack, ws : & mut WorkStack, xs : & mut WorkStack) {
         self.item.eval(ws,rs,xs);
-        eval::mul_right_dense(self.rhs.data.as_slice(), self.rhs.dim.0,self.rhs.dim.1,rs,ws,xs);
+        eval::mul_right_dense(self.rhs.data(), self.rhs.height(),self.rhs.width(),rs,ws,xs);
     }
 }
 
@@ -316,8 +363,6 @@ impl<E:ExprTrait> ExprTrait for ExprDotVec<E> {
         eval::dot_vec(self.data.as_slice(),rs,ws,xs);
     }
 }
-
-
 
 ////////////////////////////////////////////////////////////
 //
