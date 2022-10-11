4a65f4d8e679a4fb8a28b97c51b7ce26e5423c17
Ulfw <ulf.worsoe@mosek.com>
2022-10-11 16:20:32 +0200

    working on benchmark tests

--- benches/performance.rs
+++ benches/performance.rs
@@ -94,8 +94,8 @@ fn mul_sparse_matrix_x_dense_expr(bench: &mut Bencher) {
                           vec![1.0; N1*N1*2]); // cof
 
         let m = matrix::sparse(N1,N1,
-                               (1..2*N2+1).map(|v| v / 2).collect().as_slice(),
-                               (0..2*N2).map(|v| v / 2).collect().as_slice(),
+                               (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                               (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
                                vec![1.0; 2*N2].as_slice());
         m.mul(e).eval(& mut rs, & mut ws, & mut xs);
     })
@@ -108,14 +108,14 @@ fn mul_sparse_matrix_x_sparse_expr(bench: &mut Bencher) {
         let mut xs = WorkStack::new(1024);
 
         let e = Expr::new(vec![N2,N2], // dim
-                          Some((0..N2*N2).step_by(7).collect()), // sparsity
-                          (0..((N2*N2)/7+1)*2).step_by(2).collect(), // ptr
+                          Some((0..N2*N2).step_by(7).collect::<Vec<usize>>()), // sparsity
+                          (0..((N2*N2)/7+1)*2).step_by(2).collect::<Vec<usize>>(), // ptr
                           (0..((N2*N2)/7)*2).collect(), // subj
                           vec![1.0; (N2*N2/7)*2]); // cof
 
         let m = matrix::sparse(N1,N1,
-                               (1..2*N2+1).map(|v| v / 2).collect().as_slice(),
-                               (0..2*N2).map(|v| v / 2).collect().as_slice(),
+                               (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                               (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
                                vec![1.0; 2*N2].as_slice());
         m.mul(e).eval(& mut rs, & mut ws, & mut xs);
     })
@@ -134,8 +134,8 @@ fn mul_dense_expr_x_sparse_matrix(bench: &mut Bencher) {
                           vec![1.0; N1*N1*2]); // cof
 
         let m = matrix::sparse(N1,N1,
-                               (1..2*N2+1).map(|v| v / 2).collect().as_slice(),
-                               (0..2*N2).map(|v| v / 2).collect().as_slice(),
+                               (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                               (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
                                vec![1.0; 2*N2].as_slice());
         e.mul(m).eval(& mut rs, & mut ws, & mut xs);
     })
@@ -148,9 +148,9 @@ fn mul_sparse_expr_x_sparse_matrix(bench: &mut Bencher) {
         let mut xs = WorkStack::new(1024);
 
         let e = Expr::new(vec![N2,N2], // dim
-                          Some((0..N2*N2).step_by(7).collect()), // sparsity
-                          (0..((N2*N2)/7+1)*2).step_by(2).collect(), // ptr
-                          (0..((N2*N2)/7)*2).collect(), // subj
+                          Some((0..N2*N2).step_by(7).collect::<Vec<usize>>()), // sparsity
+                          (0..((N2*N2)/7+1)*2).step_by(2).collect::<Vec<usize>>(), // ptr
+                          (0..((N2*N2)/7)*2).collect::<Vec<usize>>(), // subj
                           vec![1.0; (N2*N2/7)*2]); // cof
 
         let m = matrix::sparse(N1,N1,
