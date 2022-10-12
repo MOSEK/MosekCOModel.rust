48d56c410622f8820232be8771b838a4da3f6b06
ulfw <ulf.worsoe@mosek.com>
2022-10-12 13:31:24 +0200

    switch to criterion for benchmark

--- benches/performance.rs
+++ benches/performance.rs
@@ -1,214 +1,231 @@
-#[macro_use]
-extern crate bencher;
+extern crate criterion;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 extern crate mosekmodel;
 
 use mosekmodel::*;
 use mosekmodel::expr::workstack::WorkStack;
 use mosekmodel::expr::*;
 
-use bencher::Bencher;
 
 const N1 : usize = 100;
 const N2 : usize = 500;
 
-fn mul_dense_matrix_x_dense_expr(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
+fn mul_dense_matrix_x_dense_expr(c: &mut Criterion) {
+    c.bench_function("mul_dense_matrix_x_dense_expr",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
-        let e = Expr::new(vec![N1,N1], // dim
-                          None, // sparsity
-                          (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
-                          (0..N1*N1*2).collect(), // subj
-                          vec![1.0; N1*N1*2]); // cof
+            let e = Expr::new(vec![N1,N1], // dim
+                              None, // sparsity
+                              (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
+                              (0..N1*N1*2).collect(), // subj
+                              vec![1.0; N1*N1*2]); // cof
 
-        let m = matrix::dense(N1,N1, vec![1.0; N1*N1]);
-        m.mul(e).eval(& mut rs, & mut ws, & mut xs);
-    })
+            let m = matrix::dense(N1,N1, vec![1.0; N1*N1]);
+            m.mul(e).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
-fn mul_dense_matrix_x_sparse_expr(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
+fn mul_dense_matrix_x_sparse_expr(c: &mut Criterion) {
+    c.bench_function("mul_dense_matrix_x_sparse_expr",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
-        let e = Expr::new(vec![N2,N2], // dim
-                          Some((0..N2*N2).step_by(7).collect()), // sparsity
-                          (0..((N2*N2)/7+2)*2).step_by(2).collect(), // ptr
-                          (0..((N2*N2)/7+1)*2).collect(), // subj
-                          vec![1.0; (N2*N2/7+1)*2]); // cof
+            let e = Expr::new(vec![N2,N2], // dim
+                              Some((0..N2*N2).step_by(7).collect()), // sparsity
+                              (0..((N2*N2)/7+2)*2).step_by(2).collect(), // ptr
+                              (0..((N2*N2)/7+1)*2).collect(), // subj
+                              vec![1.0; (N2*N2/7+1)*2]); // cof
 
-        let m = matrix::dense(N2,N2, vec![1.0; N2*N2]);
-        m.mul(e).eval(& mut rs, & mut ws, & mut xs);
-    })
+            let m = matrix::dense(N2,N2, vec![1.0; N2*N2]);
+            m.mul(e).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
-fn mul_dense_expr_x_dense_matrix(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
+fn mul_dense_expr_x_dense_matrix(c: &mut Criterion) {
+    c.bench_function("mul_dense_expr_x_dense_matrix",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
-        let e = Expr::new(vec![N1,N1], // dim
-                          None, // sparsity
-                          (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
-                          (0..N1*N1*2).collect(), // subj
-                          vec![1.0; N1*N1*2]); // cof
-        let m = matrix::dense(N1,N1, vec![1.0; N1*N1]);
-        e.mul(m).eval(& mut rs, & mut ws, & mut xs);
-    })
+            let e = Expr::new(vec![N1,N1], // dim
+                              None, // sparsity
+                              (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
+                              (0..N1*N1*2).collect(), // subj
+                              vec![1.0; N1*N1*2]); // cof
+            let m = matrix::dense(N1,N1, vec![1.0; N1*N1]);
+            e.mul(m).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
-fn mul_sparse_expr_x_dense_matrix(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
+fn mul_sparse_expr_x_dense_matrix(c: &mut Criterion) {
+    c.bench_function("mul_sparse_expr_x_dense_matrix",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
-        let e = Expr::new(vec![N2,N2], // dim
-                          Some((0..N2*N2).step_by(7).collect()), // sparsity
-                          (0..((N2*N2)/7+2)*2).step_by(2).collect(), // ptr
-                          (0..((N2*N2)/7+1)*2).collect(), // subj
-                          vec![1.0; (N2*N2/7+1)*2]); // cof
+            let e = Expr::new(vec![N2,N2], // dim
+                              Some((0..N2*N2).step_by(7).collect()), // sparsity
+                              (0..((N2*N2)/7+2)*2).step_by(2).collect(), // ptr
+                              (0..((N2*N2)/7+1)*2).collect(), // subj
+                              vec![1.0; (N2*N2/7+1)*2]); // cof
 
-        let m = matrix::dense(N2,N2, vec![1.0; N2*N2]);
-        e.mul(m).eval(& mut rs, & mut ws, & mut xs);
-    })
+            let m = matrix::dense(N2,N2, vec![1.0; N2*N2]);
+            e.mul(m).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
+fn mul_sparse_matrix_x_dense_expr(c: &mut Criterion) {
+    c.bench_function("mul_sparse_matrix_x_dense_expr",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
+            let e = Expr::new(vec![N1,N1], // dim
+                              None, // sparsity
+                              (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
+                              (0..N1*N1*2).collect(), // subj
+                              vec![1.0; N1*N1*2]); // cof
 
-
-fn mul_sparse_matrix_x_dense_expr(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
-
-        let e = Expr::new(vec![N1,N1], // dim
-                          None, // sparsity
-                          (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
-                          (0..N1*N1*2).collect(), // subj
-                          vec![1.0; N1*N1*2]); // cof
-
-        let m = matrix::sparse(N1,N1,
-                               (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               vec![1.0; 2*N2].as_slice());
-        m.mul(e).eval(& mut rs, & mut ws, & mut xs);
-    })
+            let m = matrix::sparse(N1,N1,
+                                   (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   vec![1.0; 2*N2].as_slice());
+            m.mul(e).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
-fn mul_sparse_matrix_x_sparse_expr(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
+fn mul_sparse_matrix_x_sparse_expr(c: &mut Criterion) {
+    c.bench_function("mul_sparse_matrix_x_sparse_expr",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
-        let e = Expr::new(vec![N2,N2], // dim
-                          Some((0..N2*N2).step_by(7).collect::<Vec<usize>>()), // sparsity
-                          (0..((N2*N2)/7+1)*2).step_by(2).collect::<Vec<usize>>(), // ptr
-                          (0..((N2*N2)/7)*2).collect(), // subj
-                          vec![1.0; (N2*N2/7)*2]); // cof
+            let e = Expr::new(vec![N2,N2], // dim
+                              Some((0..N2*N2).step_by(7).collect::<Vec<usize>>()), // sparsity
+                              (0..((N2*N2)/7+1)*2).step_by(2).collect::<Vec<usize>>(), // ptr
+                              (0..((N2*N2)/7)*2).collect(), // subj
+                              vec![1.0; (N2*N2/7)*2]); // cof
 
-        let m = matrix::sparse(N1,N1,
-                               (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               vec![1.0; 2*N2].as_slice());
-        m.mul(e).eval(& mut rs, & mut ws, & mut xs);
-    })
+            let m = matrix::sparse(N1,N1,
+                                   (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   vec![1.0; 2*N2].as_slice());
+            m.mul(e).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
-fn mul_dense_expr_x_sparse_matrix(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
+fn mul_dense_expr_x_sparse_matrix(c: &mut Criterion) {
+    c.bench_function("mul_dense_expr_x_sparse_matrix",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
-        let e = Expr::new(vec![N1,N1], // dim
-                          None, // sparsity
-                          (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
-                          (0..N1*N1*2).collect(), // subj
-                          vec![1.0; N1*N1*2]); // cof
+            let e = Expr::new(vec![N1,N1], // dim
+                              None, // sparsity
+                              (0..(N1*N1*2+2)).step_by(2).collect(), // ptr
+                              (0..N1*N1*2).collect(), // subj
+                              vec![1.0; N1*N1*2]); // cof
 
-        let m = matrix::sparse(N1,N1,
-                               (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               vec![1.0; 2*N2].as_slice());
-        e.mul(m).eval(& mut rs, & mut ws, & mut xs);
-    })
+            let m = matrix::sparse(N1,N1,
+                                   (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   vec![1.0; 2*N2].as_slice());
+            e.mul(m).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
-fn mul_sparse_expr_x_sparse_matrix(bench: &mut Bencher) {
-    bench.iter(|| {
-        let mut rs = WorkStack::new(1024);
-        let mut ws = WorkStack::new(1024);
-        let mut xs = WorkStack::new(1024);
+fn mul_sparse_expr_x_sparse_matrix(c: &mut Criterion) {
+    c.bench_function("mul_sparse_expr_x_sparse_matrix",|b|
+        b.iter(|| {
+            let mut rs = WorkStack::new(1024);
+            let mut ws = WorkStack::new(1024);
+            let mut xs = WorkStack::new(1024);
 
-        let e = Expr::new(vec![N2,N2], // dim
-                          Some((0..N2*N2).step_by(7).collect::<Vec<usize>>()), // sparsity
-                          (0..((N2*N2)/7+1)*2).step_by(2).collect::<Vec<usize>>(), // ptr
-                          (0..((N2*N2)/7)*2).collect::<Vec<usize>>(), // subj
-                          vec![1.0; (N2*N2/7)*2]); // cof
+            let e = Expr::new(vec![N2,N2], // dim
+                              Some((0..N2*N2).step_by(7).collect::<Vec<usize>>()), // sparsity
+                              (0..((N2*N2)/7+1)*2).step_by(2).collect::<Vec<usize>>(), // ptr
+                              (0..((N2*N2)/7)*2).collect::<Vec<usize>>(), // subj
+                              vec![1.0; (N2*N2/7)*2]); // cof
 
-        let m = matrix::sparse(N1,N1,
-                               (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
-                               vec![1.0; 2*N2].as_slice());
-        e.mul(m).eval(& mut rs, & mut ws, & mut xs);
-    })
-}
-
-
-
-fn bigmul(bench : &mut Bencher) {
-    bench.iter(|| {
-        const n : usize = 4096; 
-        
-        let mut model = Model::new(None);
-        let v = model.variable(None,n);
-        //let w = model.variable(None,in_psd_cone(n));
-        let mx = matrix::dense(n,n,vec![1.0; n*n]);
-
-        let _ = model.constraint(None, &mx.clone().mul(v.clone()).reshape(vec![n]),equal_to(vec![100.0;n]));
-        let _ = model.constraint(None, &v.mul(mx).reshape(vec![n]),equal_to(vec![100.0;n]));
-    })
+            let m = matrix::sparse(N1,N1,
+                                   (1..2*N2+1).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   (0..2*N2).map(|v| v / 2).collect::<Vec<usize>>().as_slice(),
+                                   vec![1.0; 2*N2].as_slice());
+            e.mul(m).eval(& mut rs, & mut ws, & mut xs);
+        })
+    );
 }
 
 
 
+fn bigmul(c: &mut Criterion) {
+    c.bench_function("bigmul",|b|
+        b.iter(|| {
+            const n : usize = 4096; 
+            
+            let mut model = Model::new(None);
+            let v = model.variable(None,n);
+            //let w = model.variable(None,in_psd_cone(n));
+            let mx = matrix::dense(n,n,vec![1.0; n*n]);
 
-fn mul_left(bench : &mut Bencher) {
+            let _ = model.constraint(None, &mx.clone().mul(v.clone()).reshape(vec![n]),equal_to(vec![100.0;n]));
+            let _ = model.constraint(None, &v.mul(mx).reshape(vec![n]),equal_to(vec![100.0;n]));
+        })
+    );
+}
+
+
+
+
+fn mul_left(c: &mut Criterion) {
     const n : usize = 256;
-    bench.iter(|| {
-        let mut model = Model::new(None);
-        let mx = matrix::dense(n,n,vec![1.0;n*n]);
+    c.bench_function("mul_left",|b|
+        b.iter(|| {
+            let mut model = Model::new(None);
+            let mx = matrix::dense(n,n,vec![1.0;n*n]);
 
-        let x = model.variable(None,vec![n,n]);
-        let y = model.variable(None,vec![n,n]);
-      
-        let _ = model.constraint(None,&mx.mul(x.add(y)),equal_to(vec![100.0; n*n]).with_shape(vec![n,n]));
-    })
+            let x = model.variable(None,vec![n,n]);
+            let y = model.variable(None,vec![n,n]);
+          
+            let _ = model.constraint(None,&mx.mul(x.add(y)),equal_to(vec![100.0; n*n]).with_shape(vec![n,n]));
+        }));
 }
 
-fn mul_right(bench : &mut Bencher) {
+fn mul_right(c: &mut Criterion) {
     const n : usize = 256;
-    bench.iter(|| {
-        let mut model = Model::new(None);
-        let mx = matrix::dense(n,n,vec![1.0;n*n]);
-        
-        let x = model.variable(None,vec![n,n]);
-        let y = model.variable(None,vec![n,n]);
+    c.bench_function("mul_right",|b|
+        b.iter(|| {
+            let mut model = Model::new(None);
+            let mx = matrix::dense(n,n,vec![1.0;n*n]);
+            
+            let x = model.variable(None,vec![n,n]);
+            let y = model.variable(None,vec![n,n]);
 
-        let _ = model.constraint(None, &x.add(y).mul(mx),equal_to(vec![100.0;n*n]).with_shape(vec![n,n]));
-    })
+            let _ = model.constraint(None, &x.add(y).mul(mx),equal_to(vec![100.0;n*n]).with_shape(vec![n,n]));
+        }));
 }
 
 
 
-benchmark_group!(benches,
+criterion_group!(benches,
                  bigmul,
                  mul_left,
                  mul_right,
@@ -216,4 +233,4 @@ benchmark_group!(benches,
                  mul_dense_matrix_x_sparse_expr,
                  mul_dense_expr_x_dense_matrix,
                  mul_sparse_expr_x_dense_matrix);
-benchmark_main!(benches);
+criterion_main!(benches);
