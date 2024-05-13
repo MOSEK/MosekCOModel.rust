a795c305a4ab828eb17d1e46d13d4f4045ee7ec1
Ulfw <ulf.worsoe@mosek.com>
2024-05-13 22:04:46 +0200

    add examples

---
+++ examples/lj-outer.rs
@@ -0,0 +1,126 @@
+//
+//  Copyright: Copyright (c) MOSEK ApS, Denmark. All rights reserved.
+// 
+//  File: lj-outer.rights
+//
+//  Computes the Löwner-John outer ellipsoid for a convex polygon. This is a 2D specialization of
+//  the more general n-dimensional case.
+//
+//
+extern crate mosekmodel;
+
+use mosekmodel::{equal_to, hstack, in_geometric_mean_cone, in_psd_cone, in_quadratic_cones, nonnegative, unbounded, vstack, Model, Sense, SolutionStatus, SolutionType, Variable};
+use mosekmodel::expr::*;
+use mosekmodel::matrix::{speye,dense};
+
+/// Purpose: Models the hypograph of the n-th power of the
+/// determinant of a positive definite matrix. See [1,2] for more details.
+///
+///   The convex set (a hypograph)
+///
+///   C = { (X, t) ∈ S^n_+ x R |  t ≤ det(X)^{1/n} },
+///
+///   can be modeled as the intersection of a semidefinite cone
+///
+///   | X   Z       |
+///   |             | ≽ 0
+///   | Z^T Diag(Z) |  
+///
+///   and a geometric mean bound
+///
+///   t <= (Z11*Z22*...*Znn)^{1/n} 
+#[allow(non_snake_case)]
+fn det_rootn(name : Option<&str>, M : &mut Model, t : Variable<0>, n : usize) -> Variable<2> {
+    // Setup variables
+    let Y = M.variable(name, in_psd_cone(2*n));
+
+    // Setup Y = [X, Z; Z^T , diag(Z)]
+    let X  = (&Y).slice(&[0..n, 0..n]);
+    let Z  = (&Y).slice(&[0..n,   n..2*n]);
+    let DZ = (&Y).slice(&[n..2*n, n..2*n]);
+
+
+    // Z is lower-triangular
+    _ = M.constraint(Some("triu(Z)=0"), &Z.clone().triuvec(false), equal_to(vec![0.0; n*(n-1)/2].as_slice()));
+    // DZ = Diag(Z)
+    _ = M.constraint(Some("DZ=Diag(Z)"), &DZ.clone().sub(Z.mul_elem(speye(n))).reshape(&[n*n]), equal_to(vec![0.0; n*n]));
+    // (Z11*Z22*...*Znn) >= t^n
+    _ = M.constraint(name,&vstack!(DZ.clone().diag(),t.reshape(&[1])), in_geometric_mean_cone(n+1));
+
+    // Return an n x n PSD variable which satisfies t <= det(X)^(1/n)
+    X
+}
+
+///  The outer ellipsoidal approximation to a polytope given
+///  as the convex hull of a set of points
+///
+///    S = conv{ x1, x2, ... , xm }
+///
+///  minimizes the volume of the enclosing ellipsoid,
+///
+///    { x | || P*x-c ||_2 <= 1 }
+///
+///  The volume is proportional to det(P)^{-1/n}, so the problem can
+///  be solved as
+///
+///    maximize         t
+///    subject to       t       <= det(P)^(1/n)
+///                || P*xi - c ||_2 <= 1,  i=1,...,m
+///                P is PSD.
+#[allow(non_snake_case)]
+fn lownerjohn_outer<const N : usize>(x : &[[f64;N]]) -> Option<(SolutionStatus,SolutionStatus,Vec<f64>,Vec<f64>)> {
+    let mut M = Model::new(Some("lownerjohn_outer"));
+        //M.setLogHandler(sys.stdout)
+        M.set_log_handler(|msg| print!("{}",msg)); 
+
+        let m = x.len();
+        let n = N;
+
+        // Setup variables
+        let t = M.variable(Some("t"), nonnegative());
+        let P = det_rootn(Some("det_rootn"),&mut M, t.clone(), n);
+        let c = M.variable(Some("c"), unbounded().with_shape(&[1,n]));
+        let x = dense(m, n, x.iter().flat_map(|row| row.iter()).cloned().collect::<Vec<f64>>());
+
+        // (1, Px-c) in cone
+        _ = M.constraint(Some("qc"),
+                         &hstack![ vec![1.0; m].into_expr().reshape(&[m,1]), 
+                                   P.clone().rev_mul(x).sub(c.clone().repeat(0,m))],
+                         in_quadratic_cones(&[m,n+1],1));
+
+        // Objective: Maximize t
+        M.objective(None,Sense::Maximize, &t);
+        M.solve();
+
+        let Psol = M.primal_solution(SolutionType::Default, &P);
+        let csol = M.primal_solution(SolutionType::Default, &c);
+        //P, c = P.level(), c.level()
+        let (psta,dsta) = M.solution_status(SolutionType::Default);
+        if let (Ok(P),Ok(c)) = (Psol,csol) {
+            Some((psta,dsta,P,c))
+        }
+        else {
+            None 
+        }
+}
+
+fn main() {
+    let points = &[[0., 0.], [1., 3.], [5.5, 4.5], [7., 4.], [7., 1.], [3., -2.]];
+
+    if let Some((psta,dsta,P,c)) = lownerjohn_outer(points) {
+        println!(" Status = {:?}/{:?}",psta,dsta);
+        println!(" P = | {:4.2} {:4.2} |, c = | {:4.2} |",P[0],P[1],c[0]);
+        println!("     | {:4.2} {:4.2} |      | {:4.2} |",P[2],P[3],c[1]);
+
+        println!("P should transform all points into the unit-ball:");
+        for p in points {
+            let (tx,ty) = (P[0]*p[0]+P[1]*p[1] - c[0],
+                           P[2]*p[0]+P[3]*p[1] - c[1]);
+            println!("\tP*{:?}-c -> ({:.4},{:.4}), norm = {:.4}",p,tx,ty,(tx*tx+ty*ty).sqrt());
+        }
+    }
+    else {
+        println!("Failed to solve");
+    }
+}
+
---
+++ examples/sdo1.rs
@@ -0,0 +1,55 @@
+//
+//  Copyright: Copyright (c) MOSEK ApS, Denmark. All rights reserved.
+//
+//  File:      sdo1.rs
+//
+//  Purpose:
+//  Solves the mixed semidefinite and conic quadratic optimization problem
+//
+//                   [2, 1, 0]
+//    minimize    Tr [1, 2, 1] * X + x0
+//                   [0, 1, 2]
+//
+//                   [1, 0, 0]
+//    subject to  Tr [0, 1, 0] * X + x0           = 1.0
+//                   [0, 0, 1]
+//
+//                   [1, 1, 1]
+//                Tr [1, 1, 1] * X      + x1 + x2 = 0.5
+//                   [1, 1, 1]
+//
+//                   X >> 0,  x0 >= (x1^2 + x2^2) ^ (1/2)
+//
+extern crate mosekmodel;
+use mosekmodel::{SolutionType,Model,Sense,equal_to,ModelItemIndex, in_psd_cone, in_quadratic_cone, matrix};
+use mosekmodel::expr::*;
+
+fn main() {
+    let mut m = Model::new(Some("sdo1"));
+    // Setting up the variables
+    let barx  = m.variable(Some("X"),  in_psd_cone(3));
+    let x     = m.variable(Some("x"),  in_quadratic_cone(3));
+
+    // Setting up constant coefficient matrices
+    let barc  = matrix::dense(3, 3, vec![2., 1., 0., 
+                                         1., 2., 1., 
+                                         0., 1., 2.]);
+    let bara1 = matrix::diag(&[1.0;3]);
+    let bara2 = matrix::ones(3, 3);
+
+    // Objective
+    m.objective(None, Sense::Minimize, &barx.clone().dot(barc).add(x.clone().index(0)));
+
+    // Constraints
+    _ = m.constraint(Some("c1"), &barx.clone().dot(bara1).add(x.clone().index(0)), equal_to(1.0));
+    _ = m.constraint(Some("c2"), &barx.clone().dot(bara2).add(x.clone().slice(&[1..3]).sum()), equal_to(0.5));
+
+
+    m.solve();
+
+    let barx_sol = m.primal_solution(SolutionType::Default,&barx);
+    let x_sol = m.primal_solution(SolutionType::Default, &x);
+
+    println!("X = {:?}",barx_sol);
+    println!("x = {:?}",x_sol);
+}
