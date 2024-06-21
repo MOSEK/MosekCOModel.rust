fe7f6f5a2a4b664ab1bbf9d38bba4473eb93e339
ulfw <ulfw@mosek.com>
2024-06-21 13:48:32 +0200

    truss: read data from file

--- examples/truss/src/main.rs
+++ examples/truss/src/main.rs
@@ -121,6 +121,8 @@ impl DrawData {
             dd.external_force[i] = f;
         }
 
+        println!("Truss:\n\t#nodes: {}\n\t#arcs: {}\n\t#forced nodes: {}",dd.points.len(),dd.arcs.len(),forces.len());
+
         dd 
     }
 }
@@ -128,43 +130,6 @@ impl DrawData {
 const D : usize = 2;
 
 pub fn main() {
-//    let mut drawdata = DrawData{
-//        points : vec![
-//            [0.0, 3.0], [0.0,1.0], [0.0,-1.0],[0.0,-3.0],
-//            [2.0, 2.0], [2.0, 0.0], [2.0, -2.0],
-//            [4.0,1.0],[4.0,-1.0],
-//            [7.0,0.0] ],
-//        node_type : vec![
-//            true,true,true,true,
-//            false,false,false,
-//            false,false,
-//            false],
-//        arcs : vec![
-//            (0,4),(0,5),
-//            (1,4),(1,5),(1,6),
-//            (2,4),(2,5),(2,6),
-//            (3,5),(3,6),
-//            (4,5),(4,7),(4,8),
-//            (5,6),(5,7),(5,8),
-//            (6,7),(6,8),
-//            (7,8),(7,9),
-//            (8,9)],
-//
-//        external_force : vec![ [0.0,0.0], 
-//                               [0.0,0.0], 
-//                               [0.0,0.0], 
-//                               [0.0,0.0], 
-//                               [0.0,0.0], 
-//                               [0.0,0.0], 
-//                               [0.0,0.0], 
-//                               [1.5,1.5], 
-//                               [0.0,0.0], 
-//                               [0.0,-2.0] ],
-//        total_material_volume : 30.0,
-//        kappa : 1.0,
-//
-//        arc_vol_stress : None,
-//    };
     let argv : Vec<String> = std::env::args().collect();
    
     let filename = if argv.len() > 1 { argv[1].as_str() } else {
@@ -281,108 +246,6 @@ fn build_ui(app   : &Application,
         .child(&darea)
         .build();
 
-//       if (false)
-//       { // Time callback
-//           let data = data.clone();
-//           let darea = darea.clone();
-//           glib::source::timeout_add_local(
-//               Duration::from_millis(10),
-//               move || {
-//                   let mut data = data.borrow_mut();
-//
-//
-//
-//                   let dt = 0.001 * (SystemTime::now().duration_since(data.t0).unwrap().as_millis() as f64);
-//
-//                   data.Abs = izip!(data.radius.iter(),data.center.iter(),data.speed.iter())
-//                       .map(|(&r,&c,&v)| {
-//                           let theta_g = (2.0 * std::f64::consts::PI * v[0] * dt * SPEED_SCALE) % (2.0 * std::f64::consts::PI);
-//                           let theta_l = (2.0 * std::f64::consts::PI * v[1] * dt * SPEED_SCALE) % (2.0 * std::f64::consts::PI);
-//
-//                           let (cost,sint) = ((theta_l/2.0).cos() , (theta_l/2.0).sin());
-//                           let A = [ cost.powi(2)*r[0]+sint.powi(2)*r[1], cost*sint*(r[1]-r[0]),
-//                                     cost*sint*(r[1]-r[0]), sint.powi(2) * r[0] + cost.powi(2) * r[1] ];
-//                           let b = [ theta_g.cos()*c[0] - theta_g.sin()*c[1],
-//                                     theta_g.sin()*c[0] + theta_g.cos()*c[1]];
-//                           (A,b)
-//                       }).collect();
-//
-//
-//                   {
-//                       // outer ellipsoid
-//                       let mut m = Model::new(None);
-//                       let t = m.variable(None, unbounded());
-//                       let p = ellipsoids::det_rootn(None, & mut m, t.clone(), 2);
-//                       let q = m.variable(None, unbounded().with_shape(&[2]));
-//
-//                       m.objective(None, mosekmodel::Sense::Maximize, &t);
-//
-//                       for (A,b) in data.Abs.iter() {
-//                           let A = DMat2::from_cols_array(A).inverse();
-//                           let b = A.mul_vec2(DVec2{x:b[0], y:b[1]}).to_array();
-//
-//                           let e : Ellipsoid<2> = ellipsoids::Ellipsoid::from_arrays(&A.to_cols_array(), &[-b[0],-b[1]]);
-//
-//                           ellipsoids::ellipsoid_contains(&mut m,&p,&q,&e);
-//                       }
-//
-//                       m.solve();
-//
-//                       if let (Ok(psol),Ok(qsol)) = (m.primal_solution(mosekmodel::SolutionType::Default,&p),
-//                                                     m.primal_solution(mosekmodel::SolutionType::Default,&q)) {
-//
-//                           // A² = P => A = sqrt(P)
-//                           // Ab = q => A\q
-//                           let s = (psol[0]*psol[3]-psol[1]*psol[2]).sqrt();
-//
-//                           let A = DMat2::from_cols_array(&[psol[0],psol[1],psol[2],psol[3]]).add_mat2(&DMat2::from_cols_array(&[s,0.0,0.0,s])).mul_scalar(1.0/(psol[0]+psol[3] + 2.0*s).sqrt());
-//                           let b = A.inverse().mul_vec2(DVec2::from_array([qsol[0],qsol[1]]));
-//
-//                           data.Pc = Some((A.to_cols_array(),b.to_array()));
-//                       }
-//                       else {
-//                           data.Pc = None;
-//                       }
-//                   }
-//
-//
-//                   {
-//                       // inner ellipsoid
-//                       let mut m = Model::new(None);
-//
-//                       let t = m.variable(None, unbounded());
-//                       let p = ellipsoids::det_rootn(None, & mut m, t.clone(), 2);
-//                       let q = m.variable(None, unbounded().with_shape(&[2]));
-//
-//                       m.objective(None, mosekmodel::Sense::Maximize, &t);
-//
-//                       for (A,b) in data.Abs.iter() {
-//                           let A = DMat2::from_cols_array(A).inverse();
-//                           let b = A.mul_vec2(DVec2{x:b[0], y:b[1]}).to_array();
-//
-//                           let e : Ellipsoid<2> = ellipsoids::Ellipsoid::from_arrays(&A.to_cols_array(), &[-b[0],-b[1]]);
-//
-//                           ellipsoids::ellipsoid_contained(&mut m,&p,&q,&e);
-//                       }
-//
-//                       m.solve();
-//
-//                       if let (Ok(psol),Ok(qsol)) = (m.primal_solution(mosekmodel::SolutionType::Default,&p),
-//                                                     m.primal_solution(mosekmodel::SolutionType::Default,&q)) {
-//                           let A = DMat2::from_cols_array(&[psol[0],psol[1],psol[2],psol[3]]).inverse();
-//                           let b = A.mul_vec2(DVec2::from_array([qsol[0],qsol[1]])).to_array();
-//
-//                           data.Qd = Some((A.to_cols_array(),[-b[0],-b[1]]));
-//                       }
-//                       else {
-//                           data.Qd = None;
-//                       }
-//                   }
-//
-//                   darea.queue_draw();
-//                   ControlFlow::Continue
-//               });
-//       }
 
     window.present();
 }
@@ -496,45 +359,5 @@ fn redraw_window(_widget : &DrawingArea, context : &Context, w : i32, h : i32, d
             context.set_matrix(mx);
         }
     }
-
-
-
-//       for (A,b) in data.Abs.iter() {
-//           context.transform(cairo::Matrix::new(A[0],A[1],A[2],A[3],b[0],b[1]));
-//           context.arc(0.0,0.0,1.0,0.0,std::f64::consts::PI*2.0);
-//           context.set_matrix(cairo::Matrix::new(1.0,0.0,0.0,1.0,0.0,0.0));
-//           _ = context.stroke();
-//           context.set_matrix(mx);
-//       }
-//
-//       if let Some((p,q)) = data.Pc {
-//           context.set_source_rgb(1.0, 0.0, 0.0);
-//
-//           let Z = DMat2::from_cols_array(&p).inverse().to_cols_array();
-//           let w = [ -Z[0] * q[0] - Z[2] * q[1],
-//                     -Z[1] * q[0] - Z[3] * q[1]];
-//
-//           context.transform(cairo::Matrix::new(Z[0], Z[1], Z[2], Z[3],w[0],w[1]));
-//           context.arc(0.0, 0.0, 1.0, 0.0, std::f64::consts::PI*2.0);
-//           context.set_matrix(cairo::Matrix::new(2.0,0.0,0.0,2.0,0.0,0.0));
-//           _ = context.stroke();
-//
-//           context.set_matrix(mx);
-//       }
-//
-//       if let Some((p,q)) = data.Qd {
-//           context.set_source_rgb(0.0, 1.0, 0.0);
-//
-//           let Z = DMat2::from_cols_array(&p).inverse().to_cols_array();
-//           let w = [ -Z[0] * q[0] - Z[2] * q[1],
-//                     -Z[1] * q[0] - Z[3] * q[1]];
-//
-//           context.transform(cairo::Matrix::new(Z[0], Z[1], Z[2], Z[3],w[0],w[1]));
-//           context.arc(0.0, 0.0, 1.0, 0.0, std::f64::consts::PI*2.0);
-//           context.set_matrix(cairo::Matrix::new(2.0,0.0,0.0,2.0,0.0,0.0));
-//           _ = context.stroke();
-//
-//           context.set_matrix(mx);
-//       }
 }
 
