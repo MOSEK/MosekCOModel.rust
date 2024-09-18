a1de4aa4afa7c2f81b0e28aee1de999f62d39f74
Ulfw <ulf.worsoe@mosek.com>
2024-09-18 09:05:00 +0200

    fix

--- condor/run_benchmarks.sh
+++ condor/run_benchmarks.sh
@@ -4,4 +4,4 @@ BD=$(cd $(dirname $0); pwd)
 export LD_LIBRARY_PATH=$BD/minidist/bin
 export PATH=/remote/public/linux/64-x86/rust/current/bin:$PATH
 
-( cd $BD/..; python3 run_benchmarks.py --rerun -s condor/result.tab )
+( cd $BD/..; python3 run_benchmarks.py --rerun --no-rebuild -s condor/result.tab )
