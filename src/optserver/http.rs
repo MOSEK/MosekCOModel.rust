c6815eaf0c6b4d8449deb526640d5b42af85d468
Ulf Worsøe <ulf.worsoe@mosek.com>
2025-07-29 21:51:18 +0200

    WIP optserver backend

--- src/optserver/http.rs
+++ src/optserver/http.rs
@@ -1,19 +1,18 @@
 //! A very basic http module, only implements just enough for simple communication with an
 //! optserver.
 //!
+//! # Usage
+//!
+//! ```
+//! fn send_and_receive(s & std::net::TcpStream) {
+//!     Request::get()_
+//!     
+//! }
+//! ```
+//!
 
 use std::net::TcpStream;
 use std::io::{Read, Write};
-use std::ops::Range;
-use std::usize;
-
-
-enum Method {
-    GET,
-    PUT,
-    POST,
-    HEAD
-}
 
 /// HTTP 1.1 request. 
 ///
@@ -29,23 +28,25 @@ enum Method {
 /// 
 ///
 #[derive(Clone)]
-struct Request {
+pub struct Request {
     header : Vec<u8>,
-
     content_length : Option<usize>,
     transfer_coding_chunked : bool,
 }
 
-struct Response<'a,T> where T : Read {
+pub struct Response<'a,T> where T : Read {
     proto_ver : (u8,u8),
     code : u16,
-    reason : Range<usize>,
+    reason : String,
     header : Vec<u8>,
-    header_start : usize,
-    header_end : usize,
+    headers : Vec<(usize,usize,usize,usize)>,
 
     chunked : bool,
+    chunk_remains : usize,
     content_length : Option<usize>,
+    readbuf : Vec<u8>,
+    pos : usize,
+    eof : bool,
 
     s : & 'a mut T,
 }
@@ -63,7 +64,7 @@ pub struct MsgWriter<'a,T> where T : std::io::Write{
 impl<'a,T> MsgWriter<'a,T> where T : std::io::Write {
     fn new(nbytes : Option<usize>, s : &'a mut T) -> MsgWriter<'a,T> 
     {
-        MsgWriter { chunked: nbytes.is_none(), remains: nbytes.unwrap_or(0), pos: 4, buffer: [0;0x1000], s }
+        MsgWriter { chunked: nbytes.is_none(), remains: nbytes.unwrap_or(0), pos: 6, buffer: [0;0x1000], s }
     }
     pub fn write(&mut self, data : &[u8]) -> std::io::Result<()> {
         if !self.chunked && data.len() > self.remains {
@@ -96,8 +97,8 @@ impl<'a,T> MsgWriter<'a,T> where T : std::io::Write {
     fn flush(&mut self) -> std::io::Result<()> {
         if self.pos > 0 {
             if self.chunked {
-                let (b0,b1) = hex_byte(((self.pos-4) & 0xff) as u8);
-                let (b2,b3) = hex_byte(((self.pos-4) >> 8) as u8);
+                let (b0,b1) = hex_byte(((self.pos-6) & 0xff) as u8);
+                let (b2,b3) = hex_byte(((self.pos-6) >> 8) as u8);
                 self.buffer[0] = b3;
                 self.buffer[1] = b2;
                 self.buffer[2] = b1;
@@ -145,12 +146,12 @@ impl Request {
     pub fn post(path : &str) -> Request { Request::new("POST", path) }
     pub fn head(path : &str) -> Request { Request::new("PUT", path) }
 
-    pub fn content_length(&mut self, size : usize) -> &mut Self{
+    pub fn content_length(mut self, size : usize) -> Self{
         self.content_length = Some(size);
         self
     }
 
-    pub fn add_header(&mut self, key : &str, value : &str) -> &mut Self{
+    pub fn add_header(mut self, key : &str, value : &str) -> Self{
         if key.eq_ignore_ascii_case("content-length") {
             if let Ok(v) = value.parse::<usize>() {
                 self.content_length = Some(v);
@@ -191,11 +192,11 @@ impl Request {
                 return Err("Mismatching Content-Length and data".to_string());
             }
             else {
-                write!(self.header,"Content-Length: {}\r\n",len);
+                write!(self.header,"Content-Length: {}\r\n",len).map_err(|e| e.to_string())?;
             }
         }
         else {
-            write!(self.header,"Content-Length: {}\r\n",data.len());
+            write!(self.header,"Content-Length: {}\r\n",data.len()).map_err(|e| e.to_string())?;
         }
         self.header.extend_from_slice(b"\r\n");
 
@@ -245,12 +246,18 @@ impl Request {
             buf
         };
         let headers_begin = buf.iter().enumerate().find(|c| *c.1 == b'\n').unwrap().1+1;
-
-        let (code,proto_ver,msg_range,hdrs_start) = {
+        let mut headers = Vec::new();
+        let mut content_length = None;
+        let mut chunked = false;
+       
+        let mut line_iter = buf.chunk_by(|c,_| *c != b'\n').scan(0,|pos,line| { let start = *pos; *pos += line.len(); if line.len() == 2 { None } else { Some((start, line)) } });
+        let (code,proto_ver,reason) = {            
+            let (_,line) = line_iter.next().ok_or_else(|| "Invalid HTTP{ status line".to_string())?;
+            println!("line = '{}'",std::str::from_utf8(line).unwrap());
             let mut code = 0u16;
             // read first line in the format "'HTTP/' [0-9] '.' [0-9]" [ ]+ [0-9]+ [ ]+ .*
-            if !buf.starts_with(b"HTTP/") { return Err("Invalid HTTP status line".to_string()); }
-            let mut it = buf[5..].iter().enumerate().peekable();
+            if !line.starts_with(b"HTTP/") { return Err("Invalid HTTP status line".to_string()); }
+            let mut it = line[5..].iter().enumerate().peekable();
             let proto_v1 = it.next().and_then(|item| match item.1 { b@(b'0'..=b'9') => Some(b-b'0'), _ => None }).ok_or("Invalid HTTP status line".to_string())?;
             if b'.' != *it.next().ok_or_else(|| "Invalid HTTP status line".to_string())?.1 { return Err("Invalid HTTP status line".to_string()); }
             let proto_v2 = it.next().and_then(|item| match item.1 { b@(b'0'..=b'9') => Some(b-b'0'), _ => None }).ok_or("Invalid HTTP status line".to_string())?;
@@ -270,45 +277,117 @@ impl Request {
             
             let message_start = it.peek().unwrap().0+5;
             let message_end = it.find(|c| *c.1 == b'\n').unwrap().0+5+1;
-            let hdrs_start = message_end+1;
+            
+            let reason = std::str::from_utf8(line[message_start..message_end].trim_ascii()).map_err(|_| "Invalid HTTP status line".to_string())?.to_string();
+    
 
-            (code,(proto_v1,proto_v2),message_start..message_end-1,hdrs_start)
+            (code,(proto_v1,proto_v2),reason)
         };
 
-        let mut resp = Response{
-            proto_ver,
-            code,
-            reason : msg_range,
-            header : buf,
-            header_start : hdrs_start,
-            header_end : lastcrlfpos-2,
+        for (start,line) in line_iter {
+            let colon_pos : usize = line.iter().enumerate().find(|&item| *item.1 == b':').ok_or_else(|| "Invalid HTTP header".to_string())?.0;
 
-            content_length : None,
-            chunked : false,
-
-            s
-        };
-
-        for line in resp.header.chunk_by(|c,_| *c == b'\n') {
-            let pos = line.iter().enumerate().find(|c| *c.1 == b':').ok_or_else(|| "Invalid HTTP header".to_string())?.0;
-            let key = &line[..pos];
-            let value = line[pos..].trim_ascii();
-
-            if      key.eq_ignore_ascii_case(b"content-length")  { 
+            let key   = &line[..colon_pos];
+            let value = &line[colon_pos+1..].trim_ascii();
+            if key.eq_ignore_ascii_case(b"content-length") { 
                 if let Some(v) = std::str::from_utf8(value).ok().and_then(|s| s.parse::<usize>().ok()) {
-                    resp.content_length = Some(v); 
+                    content_length = Some(v); 
                 }
                 else {
                     return Err("Invalid Content-Length value".to_string());
                 }
             }
-            else if key.eq_ignore_ascii_case(b"transfer-coding") { resp.chunked = value.eq_ignore_ascii_case(b"chunked"); }
+            else if key.eq_ignore_ascii_case(b"transfer-coding") { chunked = value.eq_ignore_ascii_case(b"chunked"); }
+
+            headers.push((start,start+colon_pos,start+colon_pos+1,start+line.len()))                
         }
 
-        Ok(resp)
+        let readbuf = buf[lastcrlfpos..].to_vec();
+
+        Ok(Response{
+            proto_ver,
+            code,
+            reason,
+            header : buf,
+            headers,
+
+            content_length,
+            chunked, 
+            chunk_remains : 0,
+            readbuf,
+            pos : 0,
+            eof : false,
+
+            s
+        })
     }
 }
 
+impl<'a,T> Response<'a,T> where T : Read 
+{
+    fn headers<'b>(&'b self) -> impl Iterator<Item=(&'b[u8],&'b[u8])> {
+        self.headers.iter().map(|&(k0,k1,v0,v1)| (&self.header[k0..k1],self.header[v0..v1].trim_ascii()))
+    }
+}
+
+impl<'a,T> Read for Response<'a,T> where T : Read {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        if self.chunked {
+            if self.eof { Ok(0) }
+            else if self.chunk_remains > 0 {
+                if self.pos == self.readbuf.len() {
+                    self.pos = 0;
+                    self.readbuf.resize(4096,0);
+                    let n = self.s.read(self.readbuf.as_mut_slice())?;
+                    self.readbuf.truncate(n);
+                }
+
+                let n = buf.len().min(self.readbuf.len() - self.pos).min(self.chunk_remains);
+                buf[..n].copy_from_slice(self.readbuf[self.pos..self.pos+n]);
+                self.pos += n;
+                self.chunk_remains -= n;
+                Ok(n)
+            }
+            else {
+                while self.readbuf.len()-self.pos <= 2 {
+                    if self.pos+1 == self.readbuf.len() {
+                        self.readbuf[0] = self.readbuf[self.pos];
+                        self.pos = 1;
+                    }
+                    else {
+                        self.pos = 0;
+                    }
+
+                    self.readbuf.resize(4096,0);
+                        let n = self.s.read(&mut self.readbuf[self.pos..])?;
+                        self.readbuf.truncate(n+self.pos);
+                        
+                    }
+                }
+            ...................
+
+            }
+        }
+        else {
+            if self.pos == self.readbuf.len() {
+                self.pos = 0;
+                self.readbuf.resize(4096,0);
+                let n = self.s.read(self.readbuf.as_mut_slice())?;
+                self.readbuf.truncate(n);
+            }
+
+            if self.pos < self.readbuf.len() {
+                let n = buf.len().min(self.readbuf.len()-self.pos);
+                buf[..n].copy_from_slice(&self.readbuf[self.pos..self.pos+n]);
+                self.pos += n;
+                Ok(n)
+            }
+            else {
+                Ok(0)
+            }
+        }
+    }
+}
 
 //
 // Utilities
@@ -327,6 +406,36 @@ fn hex_byte(b : u8) -> (u8,u8) {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    struct RWBuf {
+        outbuf : Vec<u8>,
+        inbuf  : Vec<u8>,
+        pos : usize,
+    }
+
+    impl RWBuf {
+        fn new(readdata : &[u8]) -> RWBuf {
+            RWBuf { outbuf: Vec::new(), inbuf: readdata.to_vec(), pos: 0 }
+        }
+    }
+
+    impl Write for RWBuf {
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            self.outbuf.write(buf)
+        }
+        fn flush(&mut self) -> std::io::Result<()> {
+            Ok(())
+        }
+    }
+    impl Read for RWBuf {
+        fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+            let n = buf.len().min(self.inbuf.len()-self.pos);
+            buf[..n].copy_from_slice(&self.inbuf[self.pos..self.pos+n]);
+            self.pos += n;
+            Ok(n)
+        }
+    }
+
     #[test]
     fn test_msg_writer() {
         {
@@ -355,9 +464,24 @@ mod test {
             w.flush().unwrap();
             w.write(b"0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789").unwrap();
             w.finalize().unwrap();
-            assert_eq!(buf.len(),213);
+            assert_eq!(buf.len(),212+5);
             assert_eq!(&buf[0..6],b"0064\r\n");
-            assert_eq!(&buf[104..108],b"64\r\n");
+            assert_eq!(&buf[106..112],b"0064\r\n");
+        }
+        {
+            let mut buf = RWBuf::new(b"HTTP/1.1 200 Ok\r\nContent-Length: 10\r\nX-Mosek-Token: SECRET\r\n\r\nabcdefghi");
+            let resp = Request::post("/path/to/something")
+                .content_length(12)
+                .add_header("Secret-Data", "SUPER SECRET")
+                .submit_data(&mut buf, b"MESSAGE BODY")
+                .unwrap();
+            for ((k,v),(kx,vx)) in resp.headers().zip([("Content-Length","10"),("X-Mosek-Token","SECRET")].iter()) {
+                assert_eq!(std::str::from_utf8(k).unwrap(),*kx);
+                assert_eq!(std::str::from_utf8(v).unwrap(),*vx);
+            }
+
+            let mut data = Vec::new();
+            resp.read()
         }
     }
 }
